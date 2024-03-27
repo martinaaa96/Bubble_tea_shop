f1cf4d228d1daf2459a780a583b3fbad740eeaf2
Martina Vangelova <mvangelovaa@abv.bg>
2024-03-27 12:34:15 +0200

    add data

---
+++ my-vite-react-app/src/utils/data.js
@@ -0,0 +1,114 @@
+export const data = [
+    {
+        id: 1,
+        name: '',
+        category: 'bubble tea',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+    },
+    {
+        id: 2,
+        name: '',
+        category: 'bubble tea',
+        ingredients: '',
+        image: '',
+        price: '12',
+
+
+    },
+    {
+        id: 3,
+        name: '',
+        category: 'bubble tea',
+        ingredients: '',
+        image: '',
+        price: '13',
+
+    },
+    {
+        id: 4,
+        name: '',
+        category: 'bubble tea',
+        ingredients: '',
+        image: '',
+        price: '8',
+
+    },
+    {
+        id: 5,
+        name: '',
+        category: 'bubble tea',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+    },
+    {
+        id: 6,
+        name: '',
+        category: 'bubble tea',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+
+    },
+    {
+        id: 7,
+        name: '',
+        category: 'Milk shake',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+    },
+    {
+        id: 8,
+        name: '',
+        category: 'milk shake',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+    },
+    {
+        id: 9,
+        name: '',
+        category: 'milk shake',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+    },
+    {
+        id: 10,
+        name: '',
+        category: 'milk shake',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+
+    },
+    {
+        id: 11,
+        name: '',
+        category: 'milk shake',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+
+    },
+    {
+        id: 12,
+        name: '',
+        category: 'milk shake',
+        ingredients: '',
+        image: '',
+        price: '10',
+
+    }
+]
