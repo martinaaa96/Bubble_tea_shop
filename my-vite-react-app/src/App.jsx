1665df4b1d19bda3ff9256db0f3965e7e0fbb314
Martina Vangelova <mvangelovaa@abv.bg>
2024-04-21 18:23:38 +0300

    add Navbar

--- my-vite-react-app/src/App.jsx
+++ my-vite-react-app/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import Header from "./components/Header";
+//import Header from "./components/Header";
 import Home from "./components/Home";
+import NavBar from "./components/Navbar";
 function App() {
   return (
     <>
-      <Header />
+      <NavBar />
       <Home />
     </>
   );
