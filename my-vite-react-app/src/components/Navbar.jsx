a2d1f35f092bfe23b31d5c9fdc6b980d720e4c95
Martina Vangelova <mvangelovaa@abv.bg>
2024-04-24 20:53:51 +0300

    update Navbar

--- my-vite-react-app/src/components/Navbar.jsx
+++ my-vite-react-app/src/components/Navbar.jsx
@@ -1,21 +1,19 @@
-export default function NavBar(){
-
-
-    return(
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-               <img src="" className="w-100 h-10 mr-2" alt="Logo" />
-           </a>
-           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-               <a className="text-base mr-5 hover:text-gray-900">Home</a>
-               <a className="text-base mr-5 hover:text-gray-900">Story</a>
-               <a className="text-base mr-5 hover:text-gray-900">Gallery</a>
-               <a className="text-base mr-5 hover:text-gray-900">Contact Us</a>
-           </nav>
-           <div className='flex mt-4 md:mt-0'>
-               <img className='pr-4' src="" />
-               <img src="" />
-           </div>
-       </div>
-    )
+export default function NavBar() {
+  return (
+    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <img src="/logo.png" className="w-100 h-20 mr-2" alt="Logo" />
+      </a>
+      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <a className="text-base mr-5 hover:text-gray-900">Home</a>
+        <a className="text-base mr-5 hover:text-gray-900">Story</a>
+        <a className="text-base mr-5 hover:text-gray-900">Gallery</a>
+        <a className="text-base mr-5 hover:text-gray-900">Contact Us</a>
+      </nav>
+      <div className="flex mt-4 md:mt-0">
+        <img className="pr-4" src="" />
+        <img src="" />
+      </div>
+    </div>
+  );
 }
