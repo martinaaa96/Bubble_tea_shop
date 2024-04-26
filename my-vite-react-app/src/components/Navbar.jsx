dce2a366e00e827c776dfe343b10cc9c081c0f6f
Martina Vangelova <mvangelovaa@abv.bg>
2024-04-26 19:05:31 +0300

    update navbar

--- my-vite-react-app/src/components/Navbar.jsx
+++ my-vite-react-app/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { LuSearch } from "react-icons/lu";
+
 export default function NavBar() {
   return (
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -11,9 +13,13 @@ export default function NavBar() {
         <a className="text-base mr-5 hover:text-gray-900">Contact Us</a>
       </nav>
       <div className="flex mt-4 md:mt-0">
-        <img className="pr-4" src="" />
-        <img src="" />
+        <LuSearch className="text-black" />
       </div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="pl-10 pr-4 py-2 rounded-lg border-none text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent"
+      />
     </div>
   );
 }
