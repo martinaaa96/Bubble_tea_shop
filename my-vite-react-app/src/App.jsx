34fbe220aa92d9cd60c246d06a790cf0f30ec3df
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-02 17:12:00 +0300

    add path for error page

--- my-vite-react-app/src/App.jsx
+++ my-vite-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 //import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 //import NavBar from "./components/Navbar";
 import Proba from "./components/Proba";
@@ -8,10 +9,13 @@ import Error from "./components/Error";
 function App() {
   return (
     <>
-      <Proba />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/*" element={<Error />} />
+      </Routes>
+
       <Footer />
-      <Error />
     </>
   );
 }
