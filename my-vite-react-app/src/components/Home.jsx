4d0bacac063c38ea192484b7135057b127d26bcf
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-10 15:21:38 +0300

    update navigation component

--- my-vite-react-app/src/components/Home.jsx
+++ my-vite-react-app/src/components/Home.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="w-screen h-[calc(100vh)]">
-      <div className="bg-cover bg-[url('')] bg-center bg-no-repeat h-full w-full style={{ backgroundSize: 'contain' }}">
-        <div className="container mx-auto flex flex-col my-auto align-middle h-full">
-          <div className="my-auto  mx-auto lg:mx-0 w-10/12 lg:w-2/5">
+      <div className="bg-cover bg-[url('/homePics.jpg')] bg-center bg-no-repeat h-full w-full style={{ backgroundSize: 'contain' }}">
+        <div className="container mx-auto flex flex-col my-auto align-middle h-full text-right">
+          <div className="my-auto mx-auto lg:mx-0 w-10/12 lg:w-2/5">
             <h1 className="text-7xl mb-4">
               <span className="text-purple-700">Bubble</span>
-              <span className="text-white"> milk </span>tea
+              <span className="text-white"> Milk </span>tea
             </h1>
-            <p className="text-2xl mb-8">Some text here </p>
-            <div className="flex items-center">
+            <p className="text-2xl mb-8">So delicious</p>
+            <div className="flex">
               <button className="rounded px-10 py-3 text-white bg-purple-700 hover:bg-purple-500">
-                Look our boba menu
+                Check our menu
               </button>
             </div>
           </div>
@@ -22,3 +22,18 @@ export default function Home() {
     </div>
   );
 }
+
+<div className="absolute top-0 w-full h-full flex flex-col justify-center text-white p-10 ">
+  <h1 className="py-4 text-3xl animate-fade-down">
+    Homemade Lemonade and Smoothies
+  </h1>
+  <h2 className="animate-fade-up ">All recipes and location store</h2>
+  <div className="flex justify-between items-center ">
+    <Link
+      to="/menu"
+      className="bg-yellow-500 hover:text-black duration-500 text-white my-7 py-2 px-4 rounded-full"
+    >
+      Menu
+    </Link>
+  </div>
+</div>;
--- my-vite-react-app/src/components/Navbar.jsx
+++ my-vite-react-app/src/components/Navbar.jsx
@@ -1,31 +1,56 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HiX} from 'react-icons/hi';
+import { RiMenu3Fill } from 'react-icons/ri';
+
 export default function NavBar() {
-  return (
-    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <Link to="/" className="my-7 md:my-0 cursor-pointer">
-          <img
-            src="/logo.png"
-            alt="logo"
-            className="hover:scale-110 duration-500"
-            style={{ width: "100px" }}
-          />
-        </Link>
-      </a>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <a className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">
-          <Link to="/">Home</Link>
-        </a>
-        <a className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">
-          <Link to="/menu">Menu</Link>
-        </a>
-        <a className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">
-          <Link to="/about">About</Link>
-        </a>
-        <a className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">
-          <Link to="/contact">Contact</Link>
-        </a>
-      </nav>
-    </div>
-  );
-}
+    const [open, setOpen] = useState(false);
+
+    const handleNav = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 font-serif">
+            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
+                <img
+                    src="/logo.png"
+                    alt="logo"
+                    className="my-7 md:my-0 cursor-pointer hover:scale-110 duration-500"
+                    style={{ width: "100px", display: open ? 'none' : 'block' }}
+                />
+            </a>
+            <nav className="hidden md:flex">
+                <Link to="/" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Home</Link>
+                <Link to="/about" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">About</Link>
+                <Link to="/menu" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Menu</Link>
+
+                <Link to="/special" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Featured products</Link>
+
+            </nav>
+            <div onClick={handleNav} className="md:hidden z-10">
+                {open ? <HiX size={20} /> : <RiMenu3Fill size={20} />}
+            </div>
+            <div
+                onClick={handleNav}
+                className={open ? "absolute left-0 top-0 w-full bg-white px-4 py-7 flex flex-col" : "absolute left-[-100%]"}
+            >
+                <ul>
+                    <li>
+                        <Link to="/" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/menu" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Menu</Link>
+                    </li>
+                    <li>
+                        <Link to="/special" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Featured products</Link>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    );
+};
