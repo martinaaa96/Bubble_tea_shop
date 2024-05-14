7e55f4114e0ee0a503c418a6e1330fdd6f4aa16e
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-14 11:22:24 +0300

    update Home

--- my-vite-react-app/src/components/Home.jsx
+++ my-vite-react-app/src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="w-screen h-[calc(100vh)]">
-      <div className="bg-cover bg-[url('/homePics.jpg')] bg-center bg-no-repeat h-full w-full style={{ backgroundSize: 'contain' }}">
+      <div className="bg-cover bg-[url('/picsHome.jpg')] bg-center bg-no-repeat h-full w-full style={{ backgroundSize: 'contain' }}">
         <div className="container mx-auto flex flex-col my-auto align-middle h-full text-right">
           <div className="my-auto mx-auto lg:mx-0 w-10/12 lg:w-2/5">
             <h1 className="text-7xl mb-4">
@@ -22,18 +22,3 @@ export default function Home() {
     </div>
   );
 }
-
-<div className="absolute top-0 w-full h-full flex flex-col justify-center text-white p-10 ">
-  <h1 className="py-4 text-3xl animate-fade-down">
-    Homemade Lemonade and Smoothies
-  </h1>
-  <h2 className="animate-fade-up ">All recipes and location store</h2>
-  <div className="flex justify-between items-center ">
-    <Link
-      to="/menu"
-      className="bg-yellow-500 hover:text-black duration-500 text-white my-7 py-2 px-4 rounded-full"
-    >
-      Menu
-    </Link>
-  </div>
-</div>;
