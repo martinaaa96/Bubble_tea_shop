11b21afbbfc5875587349f43cda370f41764f59d
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-16 19:38:03 +0300

    update nav bar links colors

--- my-vite-react-app/src/components/Home.jsx
+++ my-vite-react-app/src/components/Home.jsx
@@ -6,11 +6,11 @@ export default function Home() {
       <div className="bg-cover bg-[url('/picsHome.jpg')] bg-center bg-no-repeat h-full w-full style={{ backgroundSize: 'contain' }}">
         <div className="container mx-auto flex flex-col my-auto align-middle h-full text-right">
           <div className="my-auto mx-auto lg:mx-0 w-10/12 lg:w-2/5">
-            <h1 className="text-7xl mb-4">
+            <h1 className="text-7xl mb-7">
               <span className="text-purple-700">Bubble</span>
               <span className="text-white"> Milk </span>tea
             </h1>
-            <p className="text-2xl mb-8">So delicious</p>
+            <p className="text-2xl mb-8 text-white">So delicious</p>
             <div className="flex">
               <button className="rounded px-10 py-3 text-white bg-purple-700 hover:bg-purple-500">
                 Check our menu
--- my-vite-react-app/src/components/Navbar.jsx
+++ my-vite-react-app/src/components/Navbar.jsx
@@ -1,56 +1,146 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { HiX} from 'react-icons/hi';
-import { RiMenu3Fill } from 'react-icons/ri';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HiX } from "react-icons/hi";
+import { RiMenu3Fill } from "react-icons/ri";
+import { useLocation } from "react-router-dom";
 
 export default function NavBar() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleNav = () => {
-        setOpen(!open);
-    };
+  const handleNav = () => {
+    setOpen(!open);
+  };
 
-    return (
-        <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 font-serif">
-            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
-                <img
-                    src="/logo.png"
-                    alt="logo"
-                    className="my-7 md:my-0 cursor-pointer hover:scale-110 duration-500"
-                    style={{ width: "100px", display: open ? 'none' : 'block' }}
-                />
-            </a>
-            <nav className="hidden md:flex">
-                <Link to="/" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Home</Link>
-                <Link to="/about" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">About</Link>
-                <Link to="/menu" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Menu</Link>
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
 
-                <Link to="/special" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Featured products</Link>
-
-            </nav>
-            <div onClick={handleNav} className="md:hidden z-10">
-                {open ? <HiX size={20} /> : <RiMenu3Fill size={20} />}
-            </div>
-            <div
-                onClick={handleNav}
-                className={open ? "absolute left-0 top-0 w-full bg-white px-4 py-7 flex flex-col" : "absolute left-[-100%]"}
+  return (
+    <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 font-serif">
+      <a
+        className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        href="/"
+      >
+        <img
+          src="/logo.png"
+          alt="logo"
+          className="my-7 md:my-0 cursor-pointer hover:scale-110 duration-500"
+          style={{ width: "100px", display: open ? "none" : "block" }}
+        />
+      </a>
+      <nav className="hidden md:flex">
+        <Link
+          to="/"
+          className={
+            isHomePage
+              ? "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              : "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+          }
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className={
+            !isHomePage
+              ? "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              : "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+          }
+        >
+          About
+        </Link>
+        <Link
+          to="/menu"
+          className={
+            !isHomePage
+              ? "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              : "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+          }
+        >
+          Menu
+        </Link>
+        <Link
+          to="/special"
+          className={
+            !isHomePage
+              ? "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              : "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+          }
+        >
+          Featured products
+        </Link>
+      </nav>
+      <div onClick={handleNav} className="md:hidden z-10">
+        {open ? (
+          <HiX
+            size={20}
+            style={{ color: isHomePage ? "text-white" : "text-black" }}
+          />
+        ) : (
+          <RiMenu3Fill
+            size={20}
+            style={{ color: isHomePage ? "text-white" : "text-black" }}
+          />
+        )}
+      </div>
+      <div
+        onClick={handleNav}
+        className={
+          open
+            ? "absolute left-0 top-0 w-full bg-purple px-4 py-7 flex flex-col"
+            : "absolute left-[-100%]"
+        }
+      >
+        <ul>
+          <li>
+            <Link
+              to="/"
+              className={
+                isHomePage
+                  ? "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+                  : "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              }
             >
-                <ul>
-                    <li>
-                        <Link to="/" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/menu" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Menu</Link>
-                    </li>
-                    <li>
-                        <Link to="/special" className="text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2">Featured products</Link>
-                    </li>
-                    
-                </ul>
-            </div>
-        </div>
-    );
-};
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={
+                !isHomePage
+                  ? "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+                  : "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/menu"
+              className={
+                !isHomePage
+                  ? "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+                  : "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              }
+            >
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/special"
+              className={
+                !isHomePage
+                  ? "text-black hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+                  : "text-white hover:text-purple-700 duration-500 md:mx-4 md:my-2"
+              }
+            >
+              Featured products
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
