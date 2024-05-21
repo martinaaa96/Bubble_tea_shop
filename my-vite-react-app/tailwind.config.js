421032767ddbc72d6e72b56c2dca9077911198b3
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-21 16:14:13 +0300

    add config - fade-up and fade-right

--- my-vite-react-app/tailwind.config.js
+++ my-vite-react-app/tailwind.config.js
@@ -8,6 +8,8 @@ export default {
     extend: {
       animation: {
         'jump-in': 'jumpIn 1s ease-out',
+        'fade-up':'fadeUp 1s ease-out',
+        'fade-right': 'fadeRight 1s ease-out',
       },
       keyframes: {
         jumpIn: {
@@ -15,7 +17,16 @@ export default {
           '50%': { transform: 'scale(1.2)', opacity: 1 },
           '100%': { transform: 'scale(1)', opacity: 1 },
         },
+        fadeUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        fadeRight: {
+          '0%': { opacity: 0, transform: 'translateX(-20px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
       },
+      
     },
   },
   plugins: [],
