f2c48199c5742e91f4e6ad20e88c8373e6df590c
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-21 15:47:26 +0300

    add config animation

--- my-vite-react-app/tailwind.config.js
+++ my-vite-react-app/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'jump-in': 'jumpIn 1s ease-out',
+      },
+      keyframes: {
+        jumpIn: {
+          '0%': { transform: 'scale(0)', opacity: 0 },
+          '50%': { transform: 'scale(1.2)', opacity: 1 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 }
