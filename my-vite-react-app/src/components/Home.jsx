a7c27a2091c84e3f2a8f8cc84d300feaf863faac
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-21 15:46:58 +0300

    add animation in home ccomponent

--- my-vite-react-app/src/components/Home.jsx
+++ my-vite-react-app/src/components/Home.jsx
@@ -6,13 +6,15 @@ export default function Home() {
       <div className="bg-cover bg-[url('/picsHome.jpg')] bg-center bg-no-repeat h-full w-full style={{ backgroundSize: 'contain' }}">
         <div className="container mx-auto flex flex-col my-auto align-middle h-full text-right">
           <div className="my-auto mx-auto lg:mx-0 w-10/12 lg:w-2/5">
-            <h1 className="text-7xl mb-7">
-              <span className="text-purple-700">Bubble</span>
+            <h1 className="text-7xl mb-7 animate-jump-in">
+              <span className="text-purple-700 ">Bubble</span>
               <span className="text-white"> Milk </span>tea
             </h1>
-            <p className="text-2xl mb-8 text-white">So delicious</p>
+            <p className="text-2xl mb-8 text-white animate-jump-in ">
+              So delicious
+            </p>
             <div className="flex">
-              <button className="rounded px-10 py-3 text-white bg-purple-700 hover:bg-purple-500">
+              <button className="rounded px-10 py-3 text-white bg-purple-700 hover:scale-110 duration-700">
                 Check our menu
               </button>
             </div>
