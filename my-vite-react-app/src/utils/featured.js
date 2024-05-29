97b7e074b02c711ea8f4efdd42ea4c3233a925eb
Martina Vangelova <mvangelovaa@abv.bg>
2024-05-29 16:47:12 +0300

    create featured products

---
+++ my-vite-react-app/src/utils/featured.js
@@ -0,0 +1,24 @@
+export const featured = [
+    {
+        name: "Matcha milk tea",
+        ingredients:
+            "Matcha powder, black tea, milk, sugar, tapioca pearls, ice cubes",
+        image: "/matcha milk tea.jpg",
+        price: "14.99",
+    },
+    {
+        name: " Tiger milk tea ",
+
+        ingredients: "Brown sugar, black tea, milk, tapioca pearls, ice cubes",
+        image: "/tiger milk tea.jpg",
+        price: "12.99",
+    },
+    {
+        name: "Butterfly milk tea",
+
+        ingredients:
+            "Butterfly pea syrup, black tea, milk, sugar, tapioca pearls, ice cubes",
+        image: "/butterfly milk tea.jpg",
+        price: "15.99",
+    },
+];
